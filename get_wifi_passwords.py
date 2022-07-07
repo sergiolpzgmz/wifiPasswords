642026a436be6b7c286a4f46e50488ff3ab553b9
Sergio López Gómez <95774420+sergiolpzgmz@users.noreply.github.com>
2022-07-07 10:31:43 +0200

    Update get_wifi_passwords.py

--- get_wifi_passwords.py
+++ get_wifi_passwords.py
@@ -1,24 +1,24 @@
+#subprocess permite usar comandos de sistema
 import subprocess
-#Permite usar comandos de sistema
+#re permite usar expresiones regulares
 import re
-#Permite usar expresions regulares
 
+#Comando que lista perfiles de la interfaz WI-FI
 comando = "netsh wlan show profiles"
-#Lista perfiles de la interfaz WI-FI
 perfilInterfaz = subprocess.run(comando, capture_output=True).stdout.decode('iso-8859-1')
 #Despues de obtener la lista capturamos el nombre de la red
 nombreRed = re.findall("Perfil de todos los usuarios     : (.*)\r", perfilInterfaz)
 
-listaContraseñeas = list()
 #Lista que almacenará el nombre de la red y la contraseña
+listaContraseñeas = list()
 
+# Recorremos el listado de nombres de red para almacenarlos en un diccionario
 for nombre in nombreRed:
-    #Recorremos el listado de nombres de red para almacenarlos en un diccionario
     datosDeRed = dict()
     datosDeRed['ssid'] = nombre
 
+    #Comando que lista el perfil de la interfaz WI-FI que nos interesa mostrando la contraseña si está disponible
     segundoComando = "netsh wlan show profiles " + nombre + " key=clear"
-    #Lista el perfil de la interfaz WI-FI que nos interesa mostrando la contraseña si esta disponible
     introducirComando = subprocess.run(segundoComando, capture_output=True).stdout.decode('iso-8859-1')
     #Captura la contraseña
     contraseñaObtenida = re.findall("Contenido de la clave  : (.*)\r", introducirComando)
